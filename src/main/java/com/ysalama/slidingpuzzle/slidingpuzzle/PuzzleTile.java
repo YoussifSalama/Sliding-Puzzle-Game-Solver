0145bffc78a6c4d01093bf788875ff76c94dbe38
Youssif Salama <youssifsalama@gmail.com>
2016-12-27 13:29:35 +0200

    PuzzleTile Redundancy Removal

--- src/main/java/com/ysalama/slidingpuzzle/slidingpuzzle/PuzzleTile.java
+++ src/main/java/com/ysalama/slidingpuzzle/slidingpuzzle/PuzzleTile.java
@@ -1,7 +1,5 @@
 package com.ysalama.slidingpuzzle.slidingpuzzle;
 
-import java.awt.Point;
-
 public class PuzzleTile {
 	private int tileNumber; // The number that will display on the tile.
 	//private Point tilePosition; // The position of the tile.
